012137b8acba78e6e352003ec0fc6ec5030d4306
IanWang <ianwang3240@gmail.com>
2021-10-18 20:09:16 +0800

    Add __setattr__, __setitem__, and pickle support for Data.

--- ExaTrkXDataIO/data.py
+++ ExaTrkXDataIO/data.py
@@ -27,11 +27,33 @@ class Data:
         """
         return self.metadata[item]
 
+    def __setattr__(self, key, value):
+        """
+        Set metadata
+        """
+        self.metadata[key] = value
+
     def __getitem__(self, item):
         """
         Get dataframe.
         """
         return self.dataframes[item]
+
+    def __setitem__(self, key, value):
+        """
+        Set dataframe.
+        """
+        self.dataframes[key] = value
+
+    def __getstate__(self):
+        return {
+            'metadata': self.metadata,
+            'dataframes': self.dataframes
+        }
+
+    def __setstate__(self, state):
+        self.metadata = state['metadata']
+        self.dataframes = state['dataframes']
 
     def __repr__(self):
         str_repr = f"\n======Data======\n"
